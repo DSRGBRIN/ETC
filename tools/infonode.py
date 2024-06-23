c364494198c1aae98a52a53f857ad6a17f5d0529
DSRGBRIN <90628345+DSRGBRIN@users.noreply.github.com>
2024-06-23 18:40:58 +0700

    Create infonode.py

---
+++ tools/infonode.py
@@ -0,0 +1,35 @@
+from flask import *
+import sys
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+else:
+    import platform
+    name = platform.system() + "-" + platform.node()
+    
+app = Flask(__name__) 
+@app.route('/') 
+def home():
+	return "Welcome to the "+ name +"!!"
+
+@app.route('/info', methods = ['GET']) 
+def backup():
+    dt = []
+    from subprocess import check_output
+    if platform.system() == "Windows":
+        out = check_output(["ipconfig", "-all"]).decode("utf-8")
+    else:
+        out = check_output(["ifconfig", "-all"]).decode("utf-8")
+    stro = out.replace("\r\n", "<br>").split("<br>")
+    for i in range(len(stro)):
+        if stro[i].find("v4 Address")>0:
+            dt.append(stro[i])
+           
+    print("<br>".join(dt))
+    return "Info:<br>"+"<br>".join(dt)
+	
+if __name__ == '__main__': 
+    print("6001")
+    #app.run(host="0.0.0.0", port=6000, debug=True)
+    app.run(host="127.0.0.1", port=9999, debug=True)
+    
+    
