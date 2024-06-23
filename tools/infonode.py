fc6d30f7dcf476145610b944708dacd5231ebd9f
DSRGBRIN <90628345+DSRGBRIN@users.noreply.github.com>
2024-06-23 19:31:52 +0700

    Update infonode.py

--- tools/infonode.py
+++ tools/infonode.py
@@ -34,7 +34,7 @@ def backup():
 		
 if __name__ == '__main__': 
     print("6001")
-    #app.run(host="0.0.0.0", port=6000, debug=True)
+    #app.run(host="0.0.0.0", port=9999, debug=True)
     app.run(host="127.0.0.1", port=9999, debug=True)
     
     
