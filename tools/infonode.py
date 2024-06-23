25b689f872335e7783a464ace8d3e01d841ec23f
DSRGBRIN <90628345+DSRGBRIN@users.noreply.github.com>
2024-06-23 19:17:43 +0700

    Update infonode.py

--- tools/infonode.py
+++ tools/infonode.py
@@ -17,16 +17,21 @@ def backup():
     from subprocess import check_output
     if platform.system() == "Windows":
         out = check_output(["ipconfig", "-all"]).decode("utf-8")
+        stro = out.replace("\r\n", "<br>").split("<br>")
+        for i in range(len(stro)):
+            if stro[i].find("v4 Address")>0:
+                dt.append(stro[i])
+
     else:
         out = check_output(["ifconfig", "-a"]).decode("utf-8")
-    stro = out.replace("\r\n", "<br>").split("<br>")
-    for i in range(len(stro)):
-        if stro[i].find("v4 Address")>0:
-            dt.append(stro[i])
+        stro = out.replace("\r\n", "<br>").split("<br>")
+        for i in range(len(stro)):
+            if stro[i].find("inet")>0:
+                dt.append(stro[i])
            
     print("<br>".join(dt))
     return "Info:<br>"+"<br>".join(dt)
-	
+		
 if __name__ == '__main__': 
     print("6001")
     #app.run(host="0.0.0.0", port=6000, debug=True)
