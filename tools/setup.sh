5670b871c5c54da807b33a005dc294a33e4dc858
DSRGBRIN <90628345+DSRGBRIN@users.noreply.github.com>
2024-07-13 20:21:15 +0700

    Create setup.sh

---
+++ tools/setup.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install openjdk-17-jdk -y
+sudo apt-get install unzip
+wget https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/24.1.1/besu-24.1.1.zip
+unzip besu-24.1.1.zip
+rm besu-24.1.1.zip
+echo "Setup is Done!"
