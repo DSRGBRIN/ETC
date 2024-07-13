431adc0263797b6a4cf0716e72bff22dd9a1f176
DSRGBRIN <90628345+DSRGBRIN@users.noreply.github.com>
2024-07-13 21:02:15 +0700

    Update setup.sh

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,4 +4,6 @@ sudo apt-get install unzip
 wget https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/24.1.1/besu-24.1.1.zip
 unzip besu-24.1.1.zip
 rm besu-24.1.1.zip
+besu-24.1.1/bin/besu operator --help
+java --version
 echo "Setup is Done!"
